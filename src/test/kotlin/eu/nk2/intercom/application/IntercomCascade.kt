53762c379302acdf4e91ab1981228978a6ae75b1
nk2 <nikkoz893@gmail.com>
2020-08-26 18:41:46 +0200

    Add thread load test

--- src/test/kotlin/eu/nk2/intercom/application/IntercomCascade.kt
+++ src/test/kotlin/eu/nk2/intercom/application/IntercomCascade.kt
@@ -1,7 +1,9 @@
 package eu.nk2.intercom.application
 
 import eu.nk2.intercom.api.PublishIntercom
+import org.junit.jupiter.api.Test
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 
 interface TestInterface {
     fun testA(a: Int, b: Int): String
@@ -11,7 +13,7 @@ interface TestInterface {
     fun testB(a: String, b: Int): String
 }
 
-@Component
+@Service
 @PublishIntercom(id = TEST_INTERFACE_INTERCOM_ID)
 class TestInterfaceImpl: TestInterface {
 
@@ -29,4 +31,48 @@ class TestInterfaceImpl: TestInterface {
 
     override fun testB(a: String, b: Int): String =
         "$b$a"
+}
+
+@Service
+@PublishIntercom(id = TEST_SECOND_INTERFACE_INTERCOM_ID)
+class SecondTestInterfaceImpl: TestInterface {
+
+    override fun testA(a: Int, b: Int): String =
+        "${a*b}"
+
+    override fun testA(a: Int): String =
+        "${a*a}"
+
+    override fun testA(a: String): String =
+        "$a$a$a$a"
+
+    override fun testB(a: Int, b: String): String =
+        "$b$a$a$b"
+
+    override fun testB(a: String, b: Int): String =
+        "$b$a$a$b"
+}
+
+interface TestDelayedInterface {
+    fun test(delay: Int): Int
+}
+
+@Service
+@PublishIntercom(id = TEST_DELAYED_INTERFACE_INTERCOM_ID)
+class TestDelayedInterfaceImpl: TestDelayedInterface {
+    override fun test(delay: Int): Int {
+        Thread.sleep(delay.toLong())
+        return 1
+    }
+
+}
+
+@Service
+@PublishIntercom(id = TEST_SECOND_DELAYED_INTERFACE_INTERCOM_ID)
+class TestSecondDelayedInterfaceImpl: TestDelayedInterface {
+    override fun test(delay: Int): Int {
+        Thread.sleep(delay.toLong())
+        return 2
+    }
+
 }
--- src/test/kotlin/eu/nk2/intercom/application/IntercomTests.kt
+++ src/test/kotlin/eu/nk2/intercom/application/IntercomTests.kt
@@ -3,12 +3,24 @@ package eu.nk2.intercom.application
 import eu.nk2.intercom.api.ProvideIntercom
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.ui.context.Theme
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.CopyOnWriteArrayList
 
 @SpringBootTest(classes = [IntercomApplication::class])
 class IntercomTests {
 
     @ProvideIntercom(id = TEST_INTERFACE_INTERCOM_ID)
     private lateinit var testInterface: TestInterface
+
+    @ProvideIntercom(id = TEST_SECOND_INTERFACE_INTERCOM_ID)
+    private lateinit var secondTestInterface: TestInterface
+
+    @ProvideIntercom(id = TEST_INTERFACE_INTERCOM_ID)
+    private lateinit var testDelayedInterface: TestDelayedInterface
+
+    @ProvideIntercom(id = TEST_SECOND_INTERFACE_INTERCOM_ID)
+    private lateinit var secondTestDelayedInterface: TestDelayedInterface
 
     @Test
     fun intercomMethodWorks() {
@@ -35,5 +47,47 @@ class IntercomTests {
     @Test
     fun intercomAnotherMethodOverloadedDifferentTypeWorks() {
         assert(testInterface.testB("aaa", 4) == "4aaa") { "TestInterfaceImpl must return valid answer" }
+    }
+
+    @Test
+    fun intercomSecondInterfaceOfSameTypeWorks() {
+        assert(secondTestInterface.testA(2, 3) == "6") { "SecondTestInterfaceImpl must return valid answer" }
+        assert(secondTestInterface.testA( 3) == "9") { "SecondTestInterfaceImpl must return valid answer" }
+        assert(secondTestInterface.testA("Ko") == "KoKoKoKo") { "SecondTestInterfaceImpl must return valid answer" }
+        assert(secondTestInterface.testB(5, "Ko") == "Ko55Ko") { "SecondTestInterfaceImpl must return valid answer" }
+        assert(secondTestInterface.testB("At", 3) == "3AtAt3") { "SecondTestInterfaceImpl must return valid answer" }
+    }
+
+    @Test
+    fun intercomBothTestDelayedInterfacesWorkFromDifferentThreads() {
+        fun makeTestDelayedInterfaceThread() =
+            Thread {
+                (1..TEST_DELAYED_BENCHMARK_REPEAT_COUNT).forEach {
+                    assert(testDelayedInterface.test(TEST_DELAYED_BENCHMARK_DELAY_MS) == 1) { "TestDelayedInterfaceImpl must return valid answer" }
+                }
+            }
+
+        fun makeTestSecondDelayedInterfaceThread() =
+            Thread {
+                (1..TEST_DELAYED_BENCHMARK_REPEAT_COUNT).forEach {
+                    assert(secondTestDelayedInterface.test(TEST_DELAYED_BENCHMARK_SECOND_DELAY_MS) == 1) { "TestSecondDelayedInterfaceImpl must return valid answer" }
+                }
+            }
+
+        val threadList = CopyOnWriteArrayList<Thread>()
+        (1..TEST_DELAYED_BENCHMARK_THREAD_COUNT).forEach {
+            threadList.add(
+                makeTestDelayedInterfaceThread()
+                    .also { it.start() }
+            )
+            threadList.add(
+                makeTestSecondDelayedInterfaceThread()
+                    .also { it.start() }
+            )
+        }
+
+        threadList.forEach {
+            it.join()
+        }
     }
 }
