5bac95ad7b9f2f2ba25725fe91c6a62fa85d542b
nk2 <nikkoz893@gmail.com>
2022-02-17 13:30:04 +0100

    Throw more detailed error on tcp server exception

--- intercom/src/main/kotlin/eu/nk2/intercom/boot/stream/DefaultIntercomPublisherStreamFactory.kt
+++ intercom/src/main/kotlin/eu/nk2/intercom/boot/stream/DefaultIntercomPublisherStreamFactory.kt
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory
 import reactor.core.publisher.Mono
 import reactor.netty.DisposableServer
 import reactor.netty.tcp.TcpServer
+import java.time.Duration
 import java.util.*
 
 class DefaultIntercomPublisherStreamFactory(
@@ -27,25 +28,30 @@ class DefaultIntercomPublisherStreamFactory(
         publishers: Set<IntercomPublisher>,
         responseFactory: IntercomPublisherResponseFactory
     ) {
-        tcpServerDisposable = tcpServer
-            .doOnConnection {
-                log.debug("Connected intercom client: ${it.address()}")
-            }
-            .handle { input, output ->
-                output.sendByteArray(
-                    input.receive().asByteArray()
-                        .map { Optional.ofNullable(methodBundleSerializer.deserialize(it)) }
-                        .filter { it.isPresent }
-                        .map { it.get() }
-                        .doOnNext { log.debug("Received from intercom client ${it.publisherId}.${it.methodId}()") }
-                        .flatMap { responseFactory.buildResponse(it.publisherId, it.methodId, it.parameters) }
-                        .map { IntercomReturnBundle(error = null, data = it) }
-                        .onErrorResume(IntercomException::class.java) { Mono.just(IntercomReturnBundle(error = it.error, data = null)) }
-                        .defaultIfEmpty(IntercomReturnBundle(error = NoDataIntercomError, data = null))
-                        .map { returnBundleSerializer.serialize(it) }
-                )
-            }
-            .bindNow()
+        try {
+            tcpServerDisposable = tcpServer
+                .doOnConnection {
+                    log.debug("Connected intercom client: ${it.address()}")
+                }
+                .handle { input, output ->
+                    output.sendByteArray(
+                        input.receive().asByteArray()
+                            .map { Optional.ofNullable(methodBundleSerializer.deserialize(it)) }
+                            .filter { it.isPresent }
+                            .map { it.get() }
+                            .doOnNext { log.debug("Received from intercom client ${it.publisherId}.${it.methodId}()") }
+                            .flatMap { responseFactory.buildResponse(it.publisherId, it.methodId, it.parameters) }
+                            .map { IntercomReturnBundle(error = null, data = it) }
+                            .onErrorResume(IntercomException::class.java) { Mono.just(IntercomReturnBundle(error = it.error, data = null)) }
+                            .defaultIfEmpty(IntercomReturnBundle(error = NoDataIntercomError, data = null))
+                            .map { returnBundleSerializer.serialize(it) }
+                    )
+                }
+                .bindNow(Duration.ofMillis(1000))
+        } catch (e: Exception) {
+            log.error("Error occurred while initializing publisher stream, exiting", e)
+            throw e
+        }
     }
 
     override fun disposeStream() {
